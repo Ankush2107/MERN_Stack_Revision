6c0bfb57c23f5148f78e73bf9d759502e33aa374
Ankush2107 <ankush.kumar210701@gmail.com>
2024-03-17 19:29:09 +0530

    connected to mongodb atlas

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
-import express from 'express';
-import dotenv from 'dotenv'
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 dotenv.config();
 
-import workoutRoutes from './routes/workouts.route.js';
+import workoutRoutes from "./routes/workouts.route.js";
 
 // express app
 const app = express();
@@ -14,14 +15,22 @@ const PORT = process.env.PORT;
 app.use(express.json());
 
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-})
+  console.log(req.path, req.method);
+  next();
+});
 
 // routes
 app.use("/api/workout", workoutRoutes);
 
-// listen to request
-app.listen(PORT, () => {
-    console.log(`server is listening on http://localhost:${PORT}...`);
-})
+// connect to database
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        // listen to request
+        app.listen(PORT, () => {
+            console.log(`Connected to database and Server is listening on http://localhost:${PORT}...`)
+        })
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+
